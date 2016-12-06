a25993cfd733df8147ce18af27a651c2d05a2b3c
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2016-12-06 13:03:37 +0100

    removed unnecessary stuff

--- sick_visionary_t_driver/src/driver.cpp
+++ sick_visionary_t_driver/src/driver.cpp
@@ -1,12 +1,2 @@
 #include <ros/ros.h>
 #include <sick_visionary_t_driver/driver.h>
-
-template<>
-bool Driver_3DCS::read_param<uint8_t>(const std::string &name, uint8_t &var) {
-	int num=var;
-	if(ros::param::get("~"+name, num)) {
-		var=num;
-		return true;
-	}
-	return false;
-}
